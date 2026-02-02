7bcc2a479713a25a4c96b70646b48489662d905b
Paul Ramirez <gpaul.rl7@gmail.com>
2026-02-02 18:18:25 -0500

    exploratory work on dataset

--- src/scripts/data_collection/data_collector.py
+++ src/scripts/data_collection/data_collector.py
@@ -1,7 +1,10 @@
 import requests
+import pandas as pd
 from config.secret import GITHUB_TOKEN
 
 r = requests.get(
     "https://api.github.com/repos/godotengine/godot/issues", 
     headers={"Authorization": f"Bearer {GITHUB_TOKEN}"})
-print(r.status_code, r.json())
+df = pd.DataFrame(r.json())
+file_path = "out.json"
+df.to_json(file_path, orient="records", indent=4)
